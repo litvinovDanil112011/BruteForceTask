9f63b0c1d96252b4198a3cfcc0657925578aa74d
litvinovDanil112011 <danil112011@mail.ru>
2022-07-16 08:37:31 +0300

    add outlet.

--- Pr2503/ViewController/ViewController.swift
+++ Pr2503/ViewController/ViewController.swift
@@ -1,7 +1,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var passwordButton: UIButton!
+    @IBOutlet weak var passwordTextFild: UITextField!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var activitiIndicator: UIActivityIndicatorView!
     
     var isBlack: Bool = false {
         didSet {
@@ -21,7 +26,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         
-        self.bruteForce(passwordToUnlock: "1!gr")
+        self.bruteForce(passwordToUnlock: "1!g")
         
         // Do any additional setup after loading the view.
     }
